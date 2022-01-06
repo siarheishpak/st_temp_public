ebc88e347a3585d63bc5547cfd0d3f2d08f055fa
siarheishpak <shpaksv@gmail.com>
2022-01-05 20:34:34 -0500

    add comments

--- lissajous.py
+++ lissajous.py
@@ -7,16 +7,17 @@ def show_lissajous(x_amp, y_amp, x_freq, y_freq, x_phase, y_phase):
     x = x_amp * np.sin(x_freq * t + x_phase)
     y = y_amp * np.sin(y_freq * t + y_phase)
     fig, ax = plt.subplots()
-    ax.plot(x, y, 'b-')
-    ax.axis('equal')
+    ax.plot(x, y, 'b-') # 'b-' means solid blue line
+    ax.axis('equal') # equal axis aspect ratio
+    ax.xlabel(x oscillation)
     return fig
 
-default_text = st.empty()
-default_text.text("Select an option on the left menu")
+default_text = st.empty() # 'empty' element for the default text
+default_text.text("Select an option on the left menu") # this text will be shown if no checkboxes are selected on the left menu
 
-if st.sidebar.checkbox(label="Show Lissajous Curve", value=True):
-    default_text.empty()
-    st.subheader("Lissajous curve")
+if st.sidebar.checkbox(label="Show Lissajous Curve", value=True): # Lissajous Curve will be shown only if checkbox is selected. The checkbox is shown on the sidebar.
+    default_text.empty() # clear default text
+    st.subheader("Lissajous curve") # add subheader
     x_amp = st.sidebar.slider("Amplitude of the first oscillation", 1, 10, 2)
     y_amp = st.sidebar.slider("Amplitude of the second oscillation", 1, 10, 1)
     x_freq = st.sidebar.slider("Frequency of the first oscillation", 1, 20, 4)
