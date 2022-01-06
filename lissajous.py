2367459bf428a9bc39eee16c7e2ab8d0836e2f0a
siarheishpak <shpaksv@gmail.com>
2022-01-05 20:12:39 -0500

    add sidebar

--- lissajous.py
+++ lissajous.py
@@ -11,6 +11,9 @@ def show_lissajous(x_amp, y_amp, x_freq, y_freq, x_phase, y_phase):
     ax.axis('equal')
     return fig
 
+if st.sidebar.checkbox("Show Lissajous Curve"):
+    st.subheader("Lissajous curve")
+
 x_amp = st.slider("Amplitude of the first oscillation", 1, 10, 2)
 y_amp = st.slider("Amplitude of the second oscillation", 1, 10, 1)
 x_freq = st.slider("Frequency of the first oscillation", 1, 20, 4)
