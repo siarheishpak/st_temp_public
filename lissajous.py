80f3846af470a21faefb3698445b8eec74df2878
Siarhei Shpak <shpaksv@gmail.com>
2022-01-26 18:35:56 -0500

    1-26-2022 6-35

--- lissajous.py
+++ lissajous.py
@@ -46,4 +46,3 @@ if st.sidebar.checkbox(label="Show Lissajous Curve", value=True): # Lissajous Cu
                 st.pyplot(fig)
                 close_matplotlib_figure(fig)
             y_phase_i += 0.01
-    st.button("Hide animation")
