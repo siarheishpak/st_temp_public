a9de7eaa9f5f46c7b86ef146dddeb3428e79ec4c
Siarhei Shpak <shpaksv@gmail.com>
2022-03-04 13:37:30 -0500

    fig

--- fourier.py
+++ fourier.py
@@ -5,5 +5,6 @@ import matplotlib.pyplot as plt
 t = np.arange(256)
 sp = np.fft.fft(np.sin(t))
 freq = np.fft.fftfreq(t.shape[-1])
-plt.plot(freq, sp.real, freq, sp.imag)
-st.pyplot(plt.plot(freq, sp.real, freq, sp.imag))
+# plt.plot(freq, sp.real, freq, sp.imag)
+fig = plt.plot(freq, sp.real, freq, sp.imag)
+st.pyplot(fig)
