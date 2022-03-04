a01fd81ae03a9f090393c8855d428c6cd38ca869
Siarhei Shpak <shpaksv@gmail.com>
2022-03-04 13:54:07 -0500

    2

--- fourier.py
+++ fourier.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 t = np.arange(32)
 sp = np.fft.fft(np.sin(t))
@@ -10,3 +11,6 @@ fig, ax = plt.subplots()
 ax.plot(t, sp, 'b-') # 'b-' means solid blue line
 ax.axis('equal') # equal axis aspect ratio
 st.pyplot(fig)
+
+data = pd.DataFrame(sp)
+st.line_chart(data)
