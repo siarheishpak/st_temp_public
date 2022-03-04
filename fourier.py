1a69f243c1626e038f1e6ed5e61a04d6c875ff68
Siarhei Shpak <shpaksv@gmail.com>
2022-03-04 15:12:37 -0500

    3

--- fourier.py
+++ fourier.py
@@ -3,15 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-t = np.arange(32)
-sp = np.fft.fft(np.sin(t))
+t = np.arange(-np.pi, np.pi, 2*np.pi/256)
+f = np.cos(2*np.pi*3*t)*np.exp(-np.pi*t*t)
+sp = np.fft.fft(f)
+real = sp.real
+imag = sp.imag
+length = np.sqrt(real * real + imag * imag)
 freq = np.fft.fftfreq(t.shape[-1])
 # plt.plot(freq, sp.real, freq, sp.imag)
 fig, ax = plt.subplots()
-ax.plot(t, sp, 'b-') # 'b-' means solid blue line
-ax.axis('equal') # equal axis aspect ratio
+ax.plot(freq, sp, 'b-') # 'b-' means solid blue line
+# ax.axis('equal') # equal axis aspect ratio
 st.pyplot(fig)
-
-data = pd.DataFrame(t)
-st.line_chart(data)
-print(sp)
