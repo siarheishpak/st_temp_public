d40d010e254a4ecab3a7b49fb38a60673977ee28
Siarhei Shpak <shpaksv@gmail.com>
2022-03-04 13:46:56 -0500

    2

--- fourier.py
+++ fourier.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
-t = np.arange(256)
+t = np.arange(32)
 sp = np.fft.fft(np.sin(t))
 freq = np.fft.fftfreq(t.shape[-1])
 # plt.plot(freq, sp.real, freq, sp.imag)
